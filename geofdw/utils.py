97655e9d1dba21e2136585a0396647c150157dab
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2015-03-08 14:53:23 -0700

    working raster wkb builder

--- geofdw/utils.py
+++ geofdw/utils.py
@@ -1,8 +1,9 @@
 from multicorn import ForeignDataWrapper, Qual
 from multicorn.utils import *
 from shapely import geos
-import re
 from cStringIO import StringIO
+import re
+import struct
 
 class GeoForeignDataWrapper(ForeignDataWrapper):
   def crs_to_srid(self, crs):
@@ -32,29 +33,63 @@ class GeoRasterForeignDataWrapper(GeoForeignDataWrapper):
   def __init__(self, options, columns, srid = None):
     super(GeoRasterForeignDataWrapper, self).__init__(options, columns)    
 
-  def parse_grid(self, grid):
-    print grid
+  def parse_arcgrid(self, grid):
     buf = StringIO(grid)
     header = {}
     data = []
     while True:
       line = buf.readline().split()
+      if not line:
+        break
       word = line[0].lower()
       if word in ['ncols', 'nrows']:
         header[word] = int(line[1])
       elif word in ['xllcorner', 'yllcorner', 'cellsize', 'nodata_value']:
         header[word] = float(line[1])
       else:
-        row = []
+        if len(line) != header['ncols']:
+          raise Exception('Expected %d columns, found %d' % (len(v), header['ncols']))
         for v in line:
-          row.append(float(v))
-        if len(row) != header['ncols']:
-          raise Exception('Expected %d columns, found %d' % (len(row), header['ncols']))
-        data.append(row)
-    if len(data) != header['nrows']:
-      raise Exception('Expected %d rows, found %d' % (len(data), header['nrows']))
+          data.append(float(v))
+    return (header, data)
 
-  def grid_to_wkb(self, grid):
-    self.parse_grid(grid)
-    wkb = bytes()
+  def arcgrid_to_wkb(self, grid, bbox):
+    header, data = self.parse_arcgrid(grid)
+    width = header['ncols']
+    height = header['nrows']
+    scalex = float(bbox[2] - bbox[0]) / width
+    scaley = float(bbox[3] - bbox[1]) / height
 
+    wkb = ''
+
+    # meta data
+    wkb += struct.pack('<b', 1) # endianness
+    wkb += struct.pack('<H', 0) # version
+    wkb += struct.pack('<H', 1) # num bands
+    wkb += struct.pack('<d', scalex) # scale x
+    wkb += struct.pack('<d', scaley) # scale y
+    wkb += struct.pack('<d', bbox[0]) # upper left x
+    wkb += struct.pack('<d', bbox[3]) # upper left y
+    wkb += struct.pack('<d', 0) # upper left x
+    wkb += struct.pack('<d', 0) # upper left y
+    wkb += struct.pack('<i', self.srid) # SRID
+    wkb += struct.pack('<H', width) # width
+    wkb += struct.pack('<H', height) # height
+
+    # band meta data
+    band  = 0
+    band += 11 << 0 # pixel type (hardcoded to 64-bit floating point) TODO: select best type
+    band +=  0 << 4 # reserved bit
+    band +=  0 << 5 # is no data
+    if header['nodata_value']:
+      band +=  1 << 6 # has no data
+    else:
+      band +=  0 << 6 # has no data
+    band +=  1 << 7 # is offdb
+    wkb += struct.pack('<B', band)
+
+    # band data
+    for val in data:
+      wkb += struct.pack('<d', val)
+
+    return wkb.encode('hex')
