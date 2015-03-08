d3b7c9d5a2360a2b4ace65efbbf46a4e1a49ed8b
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2015-03-08 07:40:42 -0700

    drafting WCS support

--- geofdw/utils.py
+++ geofdw/utils.py
@@ -1,8 +1,17 @@
 from multicorn import ForeignDataWrapper, Qual
 from multicorn.utils import *
 from shapely import geos
+import re
+from cStringIO import StringIO
 
-class GeoVectorForeignDataWrapper(ForeignDataWrapper):
+class GeoForeignDataWrapper(ForeignDataWrapper):
+  def crs_to_srid(self, crs):
+    if crs == None:
+      return None
+    srid = re.sub(re.escape('epsg:'), '', crs, flags=re.I)
+    return int(srid)
+
+class GeoVectorForeignDataWrapper(GeoForeignDataWrapper):
   def __init__(self, options, columns, srid = None):
     super(GeoVectorForeignDataWrapper, self).__init__(options, columns)    
     self.use_srid(srid)
@@ -19,6 +28,33 @@ class GeoVectorForeignDataWrapper(ForeignDataWrapper):
        geos.lgeos.GEOSSetSRID(feature._geom, self.srid)
     return feature.wkb_hex
 
-class GeoRasterForeignDataWrapper(ForeignDataWrapper):
+class GeoRasterForeignDataWrapper(GeoForeignDataWrapper):
   def __init__(self, options, columns, srid = None):
     super(GeoRasterForeignDataWrapper, self).__init__(options, columns)    
+
+  def parse_grid(self, grid):
+    print grid
+    buf = StringIO(grid)
+    header = {}
+    data = []
+    while True:
+      line = buf.readline().split()
+      word = line[0].lower()
+      if word in ['ncols', 'nrows']:
+        header[word] = int(line[1])
+      elif word in ['xllcorner', 'yllcorner', 'cellsize', 'nodata_value']:
+        header[word] = float(line[1])
+      else:
+        row = []
+        for v in line:
+          row.append(float(v))
+        if len(row) != header['ncols']:
+          raise Exception('Expected %d columns, found %d' % (len(row), header['ncols']))
+        data.append(row)
+    if len(data) != header['nrows']:
+      raise Exception('Expected %d rows, found %d' % (len(data), header['nrows']))
+
+  def grid_to_wkb(self, grid):
+    self.parse_grid(grid)
+    wkb = bytes()
+
