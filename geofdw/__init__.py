6dc6eee7a0a01c2cedba0b021a1f02809703eefb
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2023-06-03 12:50:58 +0200

    path change

--- geofdw/__init__.py
+++ geofdw/__init__.py
@@ -1,4 +1,4 @@
 from multicorn import ForeignDataWrapper, Qual
 from multicorn.utils import *
-from .fdw import *
 from ._version import __version__
+from . import fdw
--- geofdw/fdw/__init__.py
+++
@@ -1,4 +0,0 @@
-from .geocode import FGeocode, RGeocode
-from .geojson import GeoJSON
-from .randompoint import RandomPoint
-from .opensky import StateVector
